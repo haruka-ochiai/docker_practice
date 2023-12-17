0337dd17076f897cd3773d2a83956ae7fdfa3c3a
haruka-ochiai <pw.g1.po1122@gmail.com>
2023-12-17 17:35:20 +0900

    add view

--- src/laravel/resources/views/layouts/app.blade.php
+++ src/laravel/resources/views/layouts/app.blade.php
@@ -13,14 +13,19 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <!-- JavaScript Bundle with Popper -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
     </head>
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+        <div class="min-h-screen bg-gray-100">
             @include('layouts.navigation')
 
             <!-- Page Heading -->
             @if (isset($header))
-                <header class="bg-white dark:bg-gray-800 shadow">
+                <header class="dark">
                     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                         {{ $header }}
                     </div>
--- src/laravel/resources/views/posts/create.blade.php
+++ src/laravel/resources/views/posts/create.blade.php
@@ -1,1 +1,34 @@
-新規登録
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="ont-semibold text-xl text-gray-800 leading-tight">
+            新規投稿
+        </h2>
+    </x-slot>
+ 
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <form method="POST" action="{{ route('posts.index') }}" enctype="multipart/form-data">
+                            @csrf
+                        <div>
+                            <label for="form-name">名前</label>
+                            <input type="text" name="name" class="form-control" id="form-name" required>
+                        </div>
+                        <div>
+                            <label for="form-description">説明文</label>
+                            <input type="text" name="description" class="form-control" id="form-description">
+                        </div>
+                        <div>
+                            <label for="form-image">画像</label><br>
+                            <input type="file" name="file" id="form-image">
+                        </div>
+                        <div class="mt-3">
+                            <button type="submit" class="btn btn-outline-info">投稿</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
